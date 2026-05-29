d5c0e7d5745914a6c10f5178b690d64d753d38d5
Thomas HUYNH <thomashuynh795@gmail.com>
2026-05-29 10:04:48 +0200

    2026-05-29 08:04:48 UTC

--- exo_2.py
+++ exo_2.py
@@ -1,8 +1,17 @@
-def map(phrase: str) -> list[str]:
-    return phrase
-    .split()
-    .replace("!", " ")
+def map_phrase(phrase: str) -> dict[str, int]:
+    lowercase_phrase: str = phrase.lower()
+    phrase_without_exclamation: str = lowercase_phrase.replace("!", " ")
+    cleaned_phrase: str = phrase_without_exclamation.replace(".", " ")
+    words: list[str] = cleaned_phrase.split()
 
+    word_counts: dict[str, int] = {}
 
-.replace(".", " ")
-.filter(x -> x > 2)
+    for word in words:
+        if len(word) > 2:
+            if word in word_counts:
+                word_counts[word] += 1
+            else:
+                word_counts[word] = 1
+
+    return word_counts
+
