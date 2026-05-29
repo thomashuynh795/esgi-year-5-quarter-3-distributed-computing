31fa1f42ad32b1eaa04491f945eecec9c0827fbb
Thomas HUYNH <thomashuynh795@gmail.com>
2026-05-29 10:56:57 +0200

    2026-05-29 08:56:57 UTC

--- exo_2.py
+++ exo_2.py
@@ -1,16 +1,34 @@
-def map_phrase(phrase: str) -> dict[str, int]:
+def map_phrase(phrase: str) -> list[tuple[str, int]]:
     lowercase_phrase: str = phrase.lower()
     phrase_without_exclamation: str = lowercase_phrase.replace("!", " ")
     cleaned_phrase: str = phrase_without_exclamation.replace(".", " ")
     words: list[str] = cleaned_phrase.split()
 
-    word_counts: dict[str, int] = {}
+    mapped_words: list[tuple[str, int]] = []
 
     for word in words:
         if len(word) > 2:
-            if word in word_counts:
-                word_counts[word] += 1
-            else:
-                word_counts[word] = 1
+            mapped_words.append((word, 1))
 
-    return word_counts
+    return mapped_words
+
+
+def shuffle_and_sort(mapped_words: list[tuple[str, int]]) -> dict[str, list[int]]:
+    grouped_words: dict[str, list[int]] = {}
+
+    for word, count in mapped_words:
+        if word in grouped_words:
+            grouped_words[word].append(count)
+        else:
+            grouped_words[word] = [count]
+
+    return grouped_words
+
+
+def reduce(grouped_words: dict[str, list[int]]) -> dict[str, int]:
+    reduced_words: dict[str, int] = {}
+
+    for word, counts in grouped_words.items():
+        reduced_words[word] = sum(counts)
+
+    return reduced_words
